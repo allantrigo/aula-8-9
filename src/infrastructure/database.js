e672ff9b74ce8607fc43d919c83642f03e4bd1b3
Allan Trigo <allantrigo@gec.inatel.br>
2022-04-28 22:36:47 -0300

    feat: add database

---
+++ src/infrastructure/database.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const uri = process.env["CONNECT_STRING"];
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const { Schema } = mongoose;
+
+const MusicSchema = new Schema({
+    id: {
+        type: String,
+        index: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    year: Number,
+    albumName: String,
+    bandName: String,
+    genre: String,
+});
+
+const MusicModel = mongoose.model('MusicModel', MusicSchema);
+
+module.exports = {
+    MusicModel,
+};
